8dcdb229339354df5b41bd5316f040e86dffab8c
Lai Boon Hui <laiboonh@gmail.com>
2022-07-01 21:06:46 +0800

    Cleanup UI for MVP

--- lib/asset_tracker_web/controllers/page_controller.ex
+++ lib/asset_tracker_web/controllers/page_controller.ex
@@ -1,7 +1,21 @@
 defmodule AssetTrackerWeb.PageController do
   use AssetTrackerWeb, :controller
 
+  alias AssetTracker.{Brokerages, Assets}
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn =
+      if get_session(conn, :user_token) != nil do
+        user = AssetTracker.Accounts.get_user_by_session_token(get_session(conn, :user_token))
+
+        conn
+        |> assign(:brokerages, Brokerages.list_brokerages(user.id))
+        |> assign(:assets, Assets.list_assets(user.id))
+      else
+        conn
+      end
+
+    conn
+    |> render("index.html")
   end
 end
